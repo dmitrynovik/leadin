f3d1b06f30a2cc602d0853de918455deea26446f
dmitrynovik <cryinstone@gmail.com>
2016-01-16 11:15:57 +1100

    Init

---
+++ leadin/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace leadin.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ leadin/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Mvc;
+
+namespace leadin.Controllers
+{
+    public class HomeController : Controller
+    {
+        private const string Title = "LeadIn Group";
+
+        public ActionResult Index()
+        {
+            ViewBag.Title = Title;
+            return View();
+        }
+
+        public ActionResult Management()
+        {
+            ViewBag.Title = Title;
+            return View();
+        }
+
+        public ActionResult ContactUs()
+        {
+            ViewBag.Title = Title;
+            return View();
+        }
+    }
+}
